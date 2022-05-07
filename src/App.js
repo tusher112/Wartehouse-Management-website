b00bdd5072834db16aa9521759a5e1a17ea3efb6
tusher112 <mdtusherahmed820@gmail.com>
2022-05-08 01:00:49 +0600

    Footer addded

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/shared/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Footer from './components/shared/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
 
     </div>
   );
---
+++ src/components/shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-dark text-center mt-2 py-3 text-white'>
+            <h5>@2022 all right reserved by @TUSHER</h5>
+        </footer>
+    );
+};
+
+export default Footer;
