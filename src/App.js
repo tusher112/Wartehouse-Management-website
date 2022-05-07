7de73e40a11fe596496da1046564a8a3df6bf30f
tusher112 <mdtusherahmed820@gmail.com>
2022-05-07 23:49:03 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <h1>hi</h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+      </Routes>
 
     </div>
   );
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">NextGen Car Warehouse</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
                         </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
+                        <Nav className='nav-link'>
+                            <Link to="/">Home</Link>
+                            <Link to="/product">Product</Link>
+                            <Link to="/blogs">Blogs</Link>
+                            <Link to="/contact">Contact</Link>
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </div>
+        </>
     );
 };
 
