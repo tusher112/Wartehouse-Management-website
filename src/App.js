fa4a3a898bf0d84985d2bddee1c0da6c9bc8fb2d
tusher112 <mdtusherahmed820@gmail.com>
2022-05-08 12:36:10 +0600

    signup component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Footer from './components/shared/Footer/Footer';
 import Login from './components/Auth/Login/Login';
+import Signup from './components/Auth/Signup/Signup';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/login" element={<Login></Login>}> </Route>
+        <Route path="/signup" element={<Signup></Signup>}> </Route>
       </Routes>
       <Footer></Footer>
 
